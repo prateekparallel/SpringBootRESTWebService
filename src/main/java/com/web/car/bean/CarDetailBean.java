3e82c8da9ae1acc17da7d38e1f4c28a90a678c4f
prateekparallel <prateekparallel@gmail.com>
2018-07-29 12:45:36 +0100

    First commit

---
+++ src/main/java/com/web/car/bean/CarDetailBean.java
@@ -0,0 +1,23 @@
+package com.web.car.bean;
+
+public class CarDetailBean {
+	private int carId;
+
+	private String carInfo;
+
+	public int getCarId() {
+		return carId;
+	}
+
+	public void setCarId(int carId) {
+		this.carId = carId;
+	}
+
+	public String getCarInfo() {
+		return carInfo;
+	}
+
+	public void setCarInfo(String carInfo) {
+		this.carInfo = carInfo;
+	}
+}
